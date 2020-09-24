0a970a9ac57f7fec59f3995baf97540c16b71bea
vinodh1988 <k.c.vinodh@hotmail.com>
2020-09-24 17:01:05 +0530

    second-commit

--- src/com/auto/AutomatedTask.java
+++ src/com/auto/AutomatedTask.java
@@ -2,6 +2,7 @@ package com.auto;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class AutomatedTask {
 
@@ -10,6 +11,10 @@ public class AutomatedTask {
 		 System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
          WebDriver wd=new ChromeDriver();
          wd.get("http://www.google.co.in");
-	}
-
+         
+         System.setProperty("webdriver.edge.driver", "D:\\msedgedriver.exe");
+         WebDriver wd1=new EdgeDriver();
+         wd1.get("http://www.google.co.in");
+         
+}
 }
