c979531587275c3e21fd8e73baebc441e11bc93d
vinodh1988 <k.c.vinodh@hotmail.com>
2020-09-25 14:58:17 +0530

    options-commit

--- src/com/auto/FileDownloadAutomation.java
+++ src/com/auto/FileDownloadAutomation.java
@@ -1,7 +1,34 @@
 package com.auto;
 
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+
 public class FileDownloadAutomation {
       public static void main(String n[]) {
+    	  String downloadFilepath = "d:\\studymaterials";
+    	  System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+    	  Map<String, Object> preferences = new Hashtable<String, Object>();
+    	  preferences.put("profile.default_content_settings.popups", 0);
+    	  preferences.put("download.prompt_for_download", "false");
+    	  preferences.put("download.default_directory", downloadFilepath);
+
     	  
+    	  preferences.put("plugins.plugins_disabled", new String[]{
+    	      "Adobe Flash Player", "Chrome PDF Viewer"});
+
+    	  ChromeOptions options = new ChromeOptions();
+    	  options.setExperimentalOption("prefs", preferences);
+    	  
+
+    	
+    	  options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+    	
+    	  WebDriver wd = new ChromeDriver(options);
+
       }
 }
