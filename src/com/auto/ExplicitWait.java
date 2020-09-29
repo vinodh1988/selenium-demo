12012f7baf7cfd3df9b9376217ccd2b511ba1c2b
vinodh1988 <k.c.vinodh@hotmail.com>
2020-09-29 17:13:50 +0530

    svg-move

--- src/com/auto/ExplicitWait.java
+++ src/com/auto/ExplicitWait.java
@@ -1,9 +1,13 @@
 package com.auto;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -20,5 +24,23 @@ public class ExplicitWait {
           
           wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div[data-reactid=\"144\"] li")));
           wd.findElement(By.cssSelector("div[data-reactid=\"144\"] li")).click();
+          
+          wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".locationcard")));
+          List<WebElement> wl=wd.findElements(By.cssSelector(".locationcard .chart-item"));
+          
+          System.out.println(wl.size());
+          for(int i=0;i<=1;i++) {
+        	  WebElement element = wl.get(i);
+        	 wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector(".locationcard .chart-item:nth-child("+(i+1)+") path"), 10));
+         	 List<WebElement> paths= element.findElements(By.xpath("//*[name()='svg']/*[name()='g']/*[@class='voronoi']/*[name()='path']"));
+             
+         	 for(WebElement path:paths) {
+             	
+          		
+          		 Actions actions = new Actions(wd);
+          		 actions.click(path).build().perform();
+          		 
+         	 }
+          }
       }
 }
